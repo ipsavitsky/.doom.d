bf4404ee0cdb1680a0f64c9bf3281f43c0d0d7f0
Ilya Savitsky <ilya.savitsky@codethink.co.uk>
2024-05-01 10:48:06 +0100

    Add lsps where possible
    Signed-off-by: Ilya Savitsky <ilya.savitsky@codethink.co.uk>

--- init.el
+++ init.el
@@ -149,7 +149,7 @@
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
        ;;lua               ; one-based indices? one-based indices
-       markdown          ; writing docs for people to ignore
+       (markdown +grip)          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
@@ -157,7 +157,7 @@
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       python            ; beautiful is better than ugly
+       (python +lsp +poetry)            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
@@ -173,7 +173,7 @@
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        web               ; the tubes
-       yaml                ; JSON, but readable
+       (yaml +lsp)                ; JSON, but readable
        ;;zig               ; C, but simpler
 
        :email
